ed02dea1e67e733bf7047f4360dd14c9c3049cbd
yanam <yanam@macbook>
2019-05-12 22:34:03 +0800

    init

--- example/src/main/java/com/yanam/example/discard/DiscardClientHandler.java
+++ example/src/main/java/com/yanam/example/discard/DiscardClientHandler.java
@@ -1,6 +1,8 @@
 package com.yanam.example.discard;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
@@ -22,4 +24,24 @@ public class DiscardClientHandler extends SimpleChannelInboundHandler<Object> {
    public void channelRead0(ChannelHandlerContext ctx,Object msg) throws Exception {
 
    }
+
+   @Override
+   public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause){
+      cause.printStackTrace();
+      ctx.close();
+   }
+
+   long counter;
+
+   private void generateTraffic(){
+      ctx.writeAndFlush(content.retainedDuplicate()).addListener(trafficGenerator);
+   }
+
+   private final ChannelFutureListener trafficGenerator = new ChannelFutureListener() {
+
+      @Override
+      public void operationComplete(ChannelFuture channelFuture) {
+        if
+      }
+   };
 }
